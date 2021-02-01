2cc9eb9e0cd09546f1f8d28bb47845d21a14d9fc
sericaer <sericaer@outlook.com>
2021-02-01 23:31:42 +0800

    优化代码

--- XUnitTest/Runner/MapTest.cs
+++ XUnitTest/Runner/MapTest.cs
@@ -21,34 +21,15 @@ namespace XUnitTest.Runner
             map.row = 5;
             map.column = 5;
 
-            map.cells = new List<ICell>()
+            map.cells = new List<ICell>();
+
+            for(int i=0; i<map.row; i++)
             {
-                Mock.Of<ICell>(x=>x.terrainKey=="T00"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T10"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T20"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T30"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T40"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T01"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T11"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T21"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T31"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T41"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T02"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T12"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T22"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T32"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T42"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T03"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T13"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T23"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T33"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T43"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T04"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T14"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T24"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T34"),
-                Mock.Of<ICell>(x=>x.terrainKey=="T44"),
-            };
+                for (int j = 0; j < map.column; j++)
+                {
+                    map.cells.Add(Mock.Of<ICell>(x => x.terrainKey == $"T{j}{i}"));
+                }
+            }
 
             var nears = map.GetNeighbours((0, 0));
             nears[DIRECTION.WEST_NORTH].Should().BeNull();
