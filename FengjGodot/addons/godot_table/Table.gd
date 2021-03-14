8115b683f7e4ca9fe6403a34b1a975f511d025f0
sericaer <sericaer@outlook.com>
2021-03-14 22:26:04 +0800

    add table

---
+++ FengjGodot/addons/godot_table/Table.gd
@@ -0,0 +1,50 @@
+tool
+extends PanelContainer
+
+# ................... Export Shared Variables ..................
+export (String, FILE, "*.tscn") var column_header_path = "res://addons/godot_table/Column/ColumnHeader.tscn"
+export (String, FILE, "*.tscn") var data_template_path = "res://addons/godot_table/Data/ButtonData.tscn"
+export (Array, String) var column_headers setget _set_column_headers
+
+export(Array, Array, String) var rows setget set_rows
+
+# Scenes and Reosurces ......................
+var preload_tableContainer : PackedScene = preload("TableContainer.tscn")
+
+# Shared Variables .........................
+var tableContainer : VBoxContainer
+var headerContainer : HeaderContainer
+var dataContainer : DataContainer
+
+func _init():
+	_init_tree()
+
+func _init_tree():
+	tableContainer = preload_tableContainer.instance()
+	self.add_child(tableContainer, true)
+	
+	headerContainer = tableContainer.get_node("HBoxContainer/HeaderContainer")
+	dataContainer = tableContainer.get_node("ScrollContainer/DataContainer")
+	
+	headerContainer.column_header_path = column_header_path
+	dataContainer.template_path = data_template_path
+	
+func _set_column_headers(new_header):
+	if column_header_path == "":
+		return
+	column_headers = new_header
+	headerContainer.clear_headers()
+	headerContainer.add_headers(column_headers)
+
+func set_rows(new_rows):
+	rows = new_rows
+	for row in new_rows:
+		var row_columns = row.size()
+		var header_columns = column_headers.size()
+		if row_columns > header_columns:
+			push_error("error")
+		elif row_columns < header_columns:
+			for index in range(header_columns - row_columns):
+				row.push_back("--")
+	dataContainer.clear_rows()
+	dataContainer.set_rows(new_rows, column_headers.size());
